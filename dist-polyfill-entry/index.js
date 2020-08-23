b44594c2d6cd3b41943c3d2f8e1efaf013be4529
zhiqiang21 <hpuhouzhiqiang@gmail.com>
2020-08-23 12:15:31 +0800

    feat:配置polyfill

---
+++ dist-polyfill-entry/index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// import "core-js";
+// import "regenerator-runtime/runtime";
+// import 'babel-polyfill'
+// map数据结构
+[1, 2, 3].map(function (n) {
+  return n + 1;
+}); // Array.prototype.includes
+
+if ([1, 2, 3].includes(2)) {
+  // Promise 对象 Javascript 内置对象
+  new Promise(function (resolve, reject) {});
+} // URLSearchParams 对象 这是一个Web API接口
+
+
+var urlp = new URLSearchParams(); // 使用class来声明类
+
+var testBird = function testBird() {
+  _classCallCheck(this, testBird);
+
+  this.state = {};
+};
+
+console.log(1);
---
+++ dist-polyfill-usage/index.js
@@ -0,0 +1,35 @@
+"use strict";
+
+require("core-js/modules/es6.promise");
+
+require("core-js/modules/es6.object.to-string");
+
+require("core-js/modules/es7.array.includes");
+
+require("core-js/modules/es6.string.includes");
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// import "core-js";
+// import "regenerator-runtime/runtime";
+// import 'babel-polyfill'
+// map数据结构
+[1, 2, 3].map(function (n) {
+  return n + 1;
+}); // Array.prototype.includes
+
+if ([1, 2, 3].includes(2)) {
+  // Promise 对象 Javascript 内置对象
+  new Promise(function (resolve, reject) {});
+} // URLSearchParams 对象 这是一个Web API接口
+
+
+var urlp = new URLSearchParams(); // 使用class来声明类
+
+var testBird = function testBird() {
+  _classCallCheck(this, testBird);
+
+  this.state = {};
+};
+
+console.log(1);
