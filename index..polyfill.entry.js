61ca823d0e1622e17793af333289d43d29cf1a11
zhiqiang21 <hpuhouzhiqiang@gmail.com>
2020-08-23 12:19:36 +0800

    feat:更新entry配置

---
+++ index..polyfill.entry.js
@@ -0,0 +1,25 @@
+// import "core-js";
+// import "regenerator-runtime/runtime";
+import 'babel-polyfill'
+
+// map数据结构
+[1, 2, 3].map((n) => n + 1);
+
+// Array.prototype.includes
+if ([1, 2, 3].includes(2)) {
+    // Promise 对象 Javascript 内置对象
+    new Promise((resolve, reject) =>{} )
+}
+
+// URLSearchParams 对象 这是一个Web API接口
+const urlp = new URLSearchParams();
+
+
+// 使用class来声明类
+class testBird {
+    constructor() {
+        this.state = {};
+    }
+}
+
+console.log(1)
